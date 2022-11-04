699dedbdcde7f2c642c1c298c636f1c0956acf2f
Kelios07 <114980626+Kelios07@users.noreply.github.com>
2022-11-04 13:09:03 +0300

    Add files via upload

---
+++ 3.11/script.js
@@ -0,0 +1,20 @@
+let test = 100;
+if(test > 101) {
+    console.log('WoW');
+} else {
+    console.log('No');
+}
+
+let a = 8;
+let b = "9";
+let c = 70;
+
+
+alert(a + b++ - c);
+
+const brain = 101;
+if (brain > 101) {
+    console.log('Decline');
+}  else {
+    console.log('Decline');
+}
